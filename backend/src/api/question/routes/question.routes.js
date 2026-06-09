bc5f88eeb261722692d898b410acfdc94c11426e
sofanitdejene-droid <sofanitdejene@gmail.com>
2026-06-09 19:41:41 +0300

    [T-09] Implement POST /api/questions and vector embedding

---
+++ backend/src/api/question/routes/question.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { authenticateUser } from "../../../middleware/authentication.js";
+import { createQuestionValidation } from "../Validations/question.validation.js"
+import { createQuestionController } from '../controller/question.controller.js'
+
+const questionRoute = express.Router();
+questionRoute.post(
+  "/",
+  authenticateUser,
+    createQuestionValidation,
+  
+  createQuestionController,
+);
+
+
+export default questionRoute;
